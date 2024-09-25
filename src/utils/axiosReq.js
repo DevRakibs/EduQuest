9d6ff882f368bc9249bf06fa2e83ee73bdbac394
hamim <hamim2114@gmail.com>
2024-09-25 13:07:58 +0600

    updated

--- src/utils/axiosReq.js
+++ src/utils/axiosReq.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-const baseURL = window.location.hostname === 'localhost'
-? 'http://localhost:5000/api'
-: 'https://edu-quest-server.vercel.app/api';
+const baseURL ='https://edu-quest-server.vercel.app/api';
 
 const axiosReq = axios.create({
   baseURL: baseURL,
-  // withCredentials: true
+  withCredentials: true
 });
 
 export default axiosReq;
